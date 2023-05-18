0b356b115b6e0c383044d47a4bac77399dd88312
Crowdbotics <engineering@crowdbotics.com>
2023-05-18 19:28:23 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "travel-chef-dev-84368",
+  url: "https://travel-chef-dev-84368.botics.co",
+  api: "https://travel-chef-dev-84368.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
